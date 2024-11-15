d794888060f6f0dffc0e6fc42f312c767c859846
Kevin Hu <kevinh64@student.ubc.ca>
2024-11-16 01:17:16 +0900

    removed blogging files

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,28 +1,7 @@
+// gatsby-node.js
 const path = require('path')
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
-  const result = await graphql(`
-    {
-      allContentfulBlogPost {
-        edges {
-          node {
-            id
-            slug
-          }
-        }
-      }
-    }
-  `)
-
-  result.data.allContentfulBlogPost.edges.forEach(({ node }) => {
-    createPage({
-      path: node.slug,
-      component: path.resolve(`./src/templates/blogPostTemplate.js`),
-      context: {
-        id: node.id,
-      },
-    })
-  })
 }
